3f84d688605fdf1e8ad21a961a73a7d5c4dce87f
Grab <apple@MacBook-Pro-3.local>
2020-12-10 10:28:01 +0900

    express 서버 구축

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/products", (req, res) => {
+  res.send({
+    products: [
+      {
+        id: 1,
+        name: "농구공",
+        price: 100000,
+        seller: "조던",
+        imageUrl: "images/products/basketball1.jpeg",
+      },
+      {
+        id: 2,
+        name: "축구공",
+        price: 50000,
+        seller: "메시",
+        imageUrl: "images/products/soccerball1.jpg",
+      },
+      {
+        id: 3,
+        name: "키보드",
+        price: 10000,
+        seller: "그랩",
+        imageUrl: "images/products/keyboard1.jpg",
+      },
+    ],
+  });
+});
+
+app.post("/products", (req, res) => {
+  res.send("상품이 등록되었습니다.");
+});
+
+app.listen(port, () => {
+  console.log("그랩의 쇼핑몰 서버가 돌아가고 있습니다");
+});
