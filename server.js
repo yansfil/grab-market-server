553eb312c2d892e068e8a6e3bbe66354eeba09b6
Grab <apple@MacBook-Pro-3.local>
2020-12-12 12:19:56 +0900

    배너 로직 추가

--- server.js
+++ server.js
@@ -19,6 +19,21 @@ app.use(express.json());
 app.use(cors());
 app.use("/uploads", express.static("uploads"));
 
+app.get("/banners", (req, res) => {
+  models.Banner.findAll({
+    limit: 2,
+  })
+    .then((result) => {
+      res.send({
+        banners: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("에러가 발생했습니다");
+    });
+});
+
 app.get("/products", (req, res) => {
   models.Product.findAll({
     order: [["createdAt", "DESC"]],
