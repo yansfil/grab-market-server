491ad01faea110624704b8774559dd02ffcecc99
Grab <apple@MacBook-Pro-3.local>
2020-12-16 23:20:47 +0900

    추천 상품 API 구현

--- index.js
+++ index.js
@@ -132,6 +132,34 @@ app.post("/purchase/:id", (req, res) => {
     });
 });
 
+app.get("/products/:id/recommendation", (req, res) => {
+  const { id } = req.params;
+  models.Product.findOne({
+    where: {
+      id,
+    },
+  })
+    .then((product) => {
+      const type = product.type;
+      models.Product.findAll({
+        where: {
+          type,
+          id: {
+            [models.Sequelize.Op.ne]: id,
+          },
+        },
+      }).then((products) => {
+        res.send({
+          products,
+        });
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("에러가 발생했습니다.");
+    });
+});
+
 app.listen(port, () => {
   console.log("그랩의 쇼핑몰 서버가 돌아가고 있습니다");
   models.sequelize
